55c9ae2e7d67e4fc9560975ebb9e92e8022f992e
Ryan <alejandr257@gmail.com>
2018-06-06 14:17:30 -0700

    Create sign in and sign up containers, attempt change from name to email on sign in

---
+++ frontend/components/session_form/session_form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class SessionForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name
+    }
+  }
+}
+
+export default sessionForm;
