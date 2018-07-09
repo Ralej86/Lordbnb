31956589dc7e2461938f567c822733fa49783182
Ryan <alejandr257@gmail.com>
2018-07-09 16:53:36 -0700

    add moment and datepicker libraries

--- frontend/components/booking/booking_form.jsx
+++ frontend/components/booking/booking_form.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
 
 class BookingForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      start_date = "",
-      end_date = "",
+      start_date = moment(),
+      end_date = moment().add(30, 'days'),
       guests: 1,
       location_id: this.props.currentLocation.id,
       guest_id: this.props.currentUser
     }
     this.handleSubmit = this.handleSubmit.bind(this)
   }
-} 
+  update(field) {
+    return (
+      e => { this.setState({[field]: e.target.value})}
+    )
+  }
+
+
+}
