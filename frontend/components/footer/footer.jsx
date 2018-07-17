80627ba26a29d1f84bc7be6161a27c00075b58cb
Ryan <alejandr257@gmail.com>
2018-07-17 10:33:35 -0700

    update css

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = state => {
   return (
     <div className="footer">
-      <p>GitubLink</p>
-      <p>LinkedIn</p>
+      <a href="https://github.com/Ralej86">Github</a>
+      <a href="https://www.linkedin.com/in/ryan-alejandro-35142a161/">LinkedIn</a>
     </div>
   )
 }
